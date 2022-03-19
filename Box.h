e599e6e54b44371dd1d00e00f02b2a76d3727567
IT21303852 <99311953+IT21303852@users.noreply.github.com>
2022-03-19 19:28:06 +0000

    Completed

--- Box.h
+++ Box.h
@@ -6,15 +6,15 @@ class Box {
     public:
         // write prototypes of setters for length, width and height
 
-        void setLength(length);
-        void setWidth(width);
-        void height(height);
+        void setLength(int l);
+        void setWidth(int w);
+        void setHeight(int h);
        
        // write prototypes of getters for length, width and height
         
-        int getLenght();
-        int getLenght();
-        int getLenght();
+        int getLength();
+        int getWidth();
+        int getHeight();
 
        int calcVolume();
 };
--- Exercise02.cpp
+++ Exercise02.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 #include "Box.h"
 
+
 int main() {
   
-   // 1. Create a Box type object called box1
+   Box box1;
   
   
    // ======= DO NOT CHANGE THE INPUT ===========
@@ -18,6 +19,10 @@ int main() {
    // ===========================================
   
    // 2. Use setters assign height, length, width throw
+
+   box1.setLength(length);
+   box1.setWidth(width);
+   box1.setHeight(height);
   
    // === DO NOT CHANGE THE OUTPUT =============
    cout << "Box Height " << box1.getHeight() << endl;
