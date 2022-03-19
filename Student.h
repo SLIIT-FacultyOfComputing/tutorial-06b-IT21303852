7f4b15ba642e484982a9a11d849dbb589e244ba1
IT21303852 <99311953+IT21303852@users.noreply.github.com>
2022-03-19 11:44:59 +0000

    Completed part 1

--- Student.h
+++ Student.h
@@ -1,9 +1,13 @@
 class Student {
-  // private section
+        private:
+            int studentId;
+            char sname[20];
   //    int studentId
   //    name <- 20 charcters
   
-  // public section
+        public:
+            void assignDetails(int studentId, char sname[]);
+            void display();
   //    assignDetails() method declaration
   //    display() method declaration
 };
